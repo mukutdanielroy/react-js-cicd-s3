03387785d6d21bafddf03056ade218b8b6cc04df
mukutdanielroy <mdanielr007@gmail.com>
2024-10-21 20:45:08 +0600

    deploy cicd

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "00007www.myreactcicd.com"
-    key            = "terraform/state.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
